b11b552ec4c1e874db91529c8d6c9f6b09cc1897
OksanaMosendz <bagheera03@gmail.com>
2021-06-01 01:42:24 +0300

    task1

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+
+
+const categoriesElements=document.querySelectorAll('li.item');
+console.log (`В списке ${categoriesElements.length} категории.`);
+
+console.log (categoriesElements);
+
+
+  console.log(document.querySelector('h2').textContent)
+  console.log()
+
+
+
+const categTitle=categoriesElements.querySelector('.item');
+
+console.log(`-Категория:${categTitle}`);
