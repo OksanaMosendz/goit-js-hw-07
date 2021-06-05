bea09d35f6a54e30767efd3d59044e43c60ce617
OksanaMosendz <bagheera03@gmail.com>
2021-06-05 23:31:35 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,34 @@
+// Напиши скрипт, который бы при потере фокуса на инпуте, проверял его содержимое
+// на правильное количество символов.
+
+// ```html
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Введи 6 символов"
+// />
+// ```
+
+// - Сколько символов должно быть в инпуте, указывается в его атрибуте
+//   `data-length`.
+// - Если введено подходящее количество, то `border`
+//  инпута становится зеленым,
+//   если неправильное - красным.
+
+// Для добавления стилей, используй CSS-классы `valid` и `invalid`.
+
+const input=document.querySelector('input');
+const length=input.dataset.length;
+const chekLength=()=>(
+  input.classList.toggle( 'valid', [input.value.length==input.dataset.length])
+  
+  input.classList.toggle( 'valid', [input.value.length!==input.dataset.length])
+  input.value.length==input.dataset.length)?(input.classList.add('valid')&input.classList.remove('invalid')):
+(input.classList.add('invalid'));
+
+input.addEventListener('blur',chekLength);
+
+// console.  (input.dataset.length);
+
+console.log (input.value.length);
